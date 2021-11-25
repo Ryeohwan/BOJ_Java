bdae11a89813747c1864b59a2f1d17cc5268aade
Ryeohwan <ahnrh951019@naver.com>
2021-11-25 12:46:00 +0900

    java_study

---
+++ 안려환 학우/BOJ_Java/src/hello/hello_world.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class hello_world {
+    public static void main(String[] args) {  // psvm 치면 빠르게 입력 가능
+        System.out.println("Hello, Java!");  // println = sout
+    }
+}
