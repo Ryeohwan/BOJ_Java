4be5091c3dd96b4a8671cc591dbecaf0d4b181ea
Ryeohwan <ahnrh951019@naver.com>
2021-11-25 12:52:26 +0900

    add comment

--- 안려환 학우/BOJ_Java/src/hello/hello_world.java
+++ 안려환 학우/BOJ_Java/src/hello/hello_world.java
@@ -3,5 +3,6 @@ package hello;
 public class hello_world {
     public static void main(String[] args) {  // psvm 치면 빠르게 입력 가능
         System.out.println("Hello, Java!");  // println = sout
+        // 땀흘림 시작
     }
 }
